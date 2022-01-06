715569ee9734fd047b862ec5fe483a2d3ae94ed4
AdamSBlack <adam@adamblack.us>
2022-01-06 00:53:37 +0000

    removed unused imports, cleaned up

---
+++ src/controllers/authentication.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+
+export async function getSession() {
+
+    const req = await axios.get(`http://localhost/retropilot/0/useradmin/session`, {withCredentials: true});
+    return req.data
+
+}
