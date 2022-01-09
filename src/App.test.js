7b0ce6269c23adad92ab2cd46fadc36e4dd7b4d7
Cameron Clough <cameronjclough@gmail.com>
2022-01-09 23:00:37 +0000

    lint: allow non-jsx file extensions

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import { render } from '@testing-library/react';
 
 test('renders learn react link', () => {
   render(<p />);
