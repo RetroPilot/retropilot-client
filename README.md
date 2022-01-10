06c7cd01b39823d3020b4766a0514df38ab93209
Cameron Clough <cameronjclough@gmail.com>
2022-01-10 02:13:00 +0000

    dotenv config

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ React client for Retropilot
 
 
 Ties into https://github.com/retropilot/retropilot-server/ to provide a react frontend
+
+To override the environment variables, create a `.env.local` file. You can also override the
+variables for development, testing and production independently (`.env.development`,
+`.env.test.local` and `.env.production.local` files).
---
+++ src/config.js
@@ -0,0 +1,18 @@
+const config = {
+  /**
+   * The address at which the website is hosted.
+   */
+  publicUrl: process.env.PUBLIC_URL,
+
+  /**
+   * The address at which the API is accessible.
+   */
+  apiUrl: process.env.API_URL,
+
+  /**
+   * The name displayed in the website title.
+   */
+  websiteName: process.env.REACT_APP_WEBSITE_NAME,
+};
+
+export default config;
