f722aec648427e9cf25a5c6eef103360769f8c40
Cameron Clough <cameronjclough@gmail.com>
2022-03-21 23:40:12 +0000

    fix env/config names

--- src/config.js
+++ src/config.js
@@ -2,17 +2,19 @@ const config = {
   /**
    * The address at which the website is hosted.
    */
-  publicUrl: process.env.PUBLIC_URL,
+  publicUrl: process.env.REACT_APP_PUBLIC_URL,
 
   /**
    * The address at which the API is accessible.
    */
-  apiUrl: process.env.API_URL,
+  apiUrl: process.env.REACT_APP_API_URL,
 
   /**
    * The name displayed in the website title.
    */
   websiteName: process.env.REACT_APP_WEBSITE_NAME,
 };
+
+console.log({ config });
 
 export default config;
